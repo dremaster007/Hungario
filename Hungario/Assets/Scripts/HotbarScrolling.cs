efac6234ed4df80ffeb5138e82e6869f8fdbd36f
Tony Matos <matosman1209@gmail.com>
2018-05-31 22:17:43 -0400

    Stat manager script added

--- Hungario/Assets/Scripts/HotbarScrolling.cs
+++ Hungario/Assets/Scripts/HotbarScrolling.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HotKeyNumbers
-{
-    
-}
-
 public class HotbarScrolling : MonoBehaviour
 {
     public Scrollbar sb;
-    public float scroll;
+    //public float scroll;
 
     public void Start()
     {
         sb.GetComponent<Scrollbar>().value = 0;
 
-        scroll = Input.GetAxis("Mouse ScrollWheel");
+        //scroll = Input.GetAxis("Mouse ScrollWheel");
     }
 
     public void Update()
     {
         #region Scroll Wheel
-        if(scroll > 0f)
+        /*if(scroll > 0f)
         {
             Debug.Log("Scrolling Up.");
             sb.value++;
@@ -43,6 +38,59 @@ public class HotbarScrolling : MonoBehaviour
         if (sb.value < 0)
         {
             sb.value = 10;
+        }*/      
+        #endregion
+
+        #region Scroll Hotkeys
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sb.value = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sb.value = .1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sb.value = .2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            sb.value = .3f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            sb.value = .4f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Debug.Log("Alpha 6");
+            sb.value = .51f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            sb.value = .63f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            sb.value = .75f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            sb.value = .87f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            sb.value = .99f;
         }
         #endregion
     }
---
+++ Hungario/Assets/Scripts/StatManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    public int health = 100;
+    public int food = 100;
+    public int water = 100;
+    public int cold = 100;
+
+
+}
