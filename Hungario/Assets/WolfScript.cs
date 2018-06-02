9d52436bea2021112cb5a8dee725c1aa96ce0f74
zacharylawrence00 <33839205+zacharylawrence00@users.noreply.github.com>
2018-06-02 17:49:16 -0400

    Wolf Script Added and Enemy Prefab

---
+++ Hungario/Assets/WolfScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfScript : MonoBehaviour {
+
+    public int attackDamage = 20;
+
+    public float speed = 7f;
+    GameObject player;
+
+    public bool playerInRange;
+
+    public void Update()
+    {
+        if(playerInRange)
+        {
+            StartCoroutine(ChaseThePlayer(player));
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D theCollision)
+    {
+        if (theCollision.gameObject.tag == "Player")
+        {
+            player = theCollision.gameObject;
+            playerInRange = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D theCollision)
+    {
+        if(theCollision.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    IEnumerator ChaseThePlayer(GameObject theCollision)
+    {
+        yield return new WaitForSeconds(1f);
+        transform.position = Vector3.MoveTowards(transform.position, theCollision.transform.position, speed * Time.deltaTime);
+    }
+}
