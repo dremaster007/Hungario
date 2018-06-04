c26f4acd502bcea34b6e0ca8ed56443a9820d053
RespectTeFallen <39815526+RespectTeFallen@users.noreply.github.com>
2018-06-03 18:59:50 -0700

    EnemyTestingAIPaul

---
+++ Hungario/Assets/Scripts/EnemyAITestPaul.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAITestPaul : MonoBehaviour {
+
+    [SerializeField]
+    GameObject target;
+    Vector3 velocity;
+    bool chase = false;
+    float angle;
+    public float speed;
+    bool toggle = true;
+
+    private void Update()
+    {
+        if (chase)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, target.transform.position) > 1)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "PlayerTDS")
+        {
+            if (toggle)
+            {
+                chase = true;
+            }
+            else if (!toggle)
+            {
+                chase = false;
+            }
+        }
+    }
+}
---
+++ Hungario/Assets/Scripts/HurtPlayerAITestPaul.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayerAITestPaul : MonoBehaviour {
+
+    bool tickDamage = false;
+    bool delayTick = false;
+
+    private void Update()
+    {
+        if (tickDamage && !delayTick)
+        {
+            delayTick = true;
+            Debug.Log("HitPlayer");
+            StartCoroutine(TickDelay());
+        }
+    }
+
+    IEnumerator TickDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        delayTick = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PlayerTDS")
+        {
+            tickDamage = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PlayerTDS")
+        {
+            tickDamage = false;
+        }
+    }
+}
